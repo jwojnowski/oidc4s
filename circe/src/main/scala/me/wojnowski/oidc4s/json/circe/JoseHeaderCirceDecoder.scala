691255e9851e2f5e1ced2d974ee2bcf16aaa7b24
Jakub Wojnowski <29680262+jwojnowski@users.noreply.github.com>
2023-11-04 10:37:15 +0100

    ♻️ 👌 Encapsulate UnsupportedAlgorithm raw error prefix.

--- circe/src/main/scala/me/wojnowski/oidc4s/json/circe/JoseHeaderCirceDecoder.scala
+++ circe/src/main/scala/me/wojnowski/oidc4s/json/circe/JoseHeaderCirceDecoder.scala
@@ -2,6 +2,7 @@ package me.wojnowski.oidc4s.json.circe
 
 import io.circe.Decoder
 import me.wojnowski.oidc4s.Algorithm
+import me.wojnowski.oidc4s.IdTokenVerifier.Error.UnsupportedAlgorithm
 import me.wojnowski.oidc4s.JoseHeader
 import me.wojnowski.oidc4s.json.JsonSupport
 
@@ -9,7 +10,7 @@ trait JoseHeaderCirceDecoder {
 
   private implicit val jwtAlgorithmCirceDecoder: Decoder[Algorithm] =
     Decoder[String].emap { shortName =>
-      Algorithm.findByShortName(shortName).toRight(s"${JsonSupport.unsupportedAlgorithmErrorPrefix}$shortName")
+      Algorithm.findByShortName(shortName).toRight(UnsupportedAlgorithm(shortName).toRawError)
     }
 
   protected implicit val joseHeaderCirceDecoder: Decoder[JoseHeader] =
